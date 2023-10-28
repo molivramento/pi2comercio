aef15a82d0c74d731f8363d26fd1c20433910945
moliv <molivramento@gmail.com>
2023-10-28 11:48:14 -0300

    added product router

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from fastapi import FastAPI
+from database import config_database
+
+from app.products.api import router as product_router
+
+config_database()
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+app.include_router(product_router, prefix='/products', tags=['Product'])
