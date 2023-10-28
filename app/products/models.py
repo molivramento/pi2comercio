0c6174e6fa56bc37051f84567f7d2816c1487d8a
moliv <molivramento@gmail.com>
2023-10-28 11:48:37 -0300

    added models.py product

---
+++ app/products/models.py
@@ -0,0 +1,14 @@
+import ormar
+from uuid import UUID
+from database import BaseMeta
+
+
+class Product(ormar.Model):
+    class Meta(BaseMeta):
+        tablename = "products"
+
+    id: UUID = ormar.UUID(primary_key=True, editable=False)
+    name: str = ormar.String(max_length=64)
+    description: str = ormar.String(max_length=256)
+    price: float = ormar.Float()
+    quantity: int = ormar.Integer()
