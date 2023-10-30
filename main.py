9ea12516a81e88a601f478682c00cdbf1282a90f
moliv <molivramento@gmail.com>
2023-10-30 09:38:23 -0300

    added CORS

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from database import config_database
 
 from app.products.api import router as product_router
@@ -8,3 +10,12 @@ config_database()
 app = FastAPI()
 
 app.include_router(product_router, prefix='/products', tags=['Product'])
+
+origins = ['http://localhost:9000',
+           'http://127.0.0.1:9000']
+
+app.add_middleware(CORSMiddleware,
+                   allow_origins=origins,
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
