59cbf716c8a25f75535c0f093d6781f306e9c441
Marcelo Livramento <molivramento@gmail.com>
2023-11-03 04:40:31 -0300

    test boto3 aws bucket

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import os
+
 import boto3
-from fastapi import FastAPI
+from dotenv import load_dotenv
+from fastapi import FastAPI, UploadFile
 from fastapi.middleware.cors import CORSMiddleware
 
 from database import config_database
@@ -21,3 +24,19 @@ app.add_middleware(CORSMiddleware,
                    allow_methods=["*"],
                    allow_headers=["*"])
 
+# TODO: TEST AWS BUCKET (BOTO3)
+load_dotenv()
+
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+bucket_name = os.getenv('BUCKET_NAME')
+
+s3 = boto3.client('s3',
+                  aws_access_key_id=aws_access_key_id,
+                  aws_secret_access_key=aws_secret_access_key)
+
+
+@app.post('/')
+async def upload_file(file: UploadFile = None):
+    s3.upload_fileobj(file.file, bucket_name, file.filename)
+    return file.filename
