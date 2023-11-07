dbca2c3189b65dd26c5f8bcec344d14ae3ea1da1
moliv <molivramento@gmail.com>
2023-11-07 18:57:11 -0300

    fix: render

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-
-import boto3
 from dotenv import load_dotenv
-from fastapi import FastAPI, UploadFile
+from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from database import config_database
 
@@ -11,7 +10,10 @@ from app.api.products import router as product_router
 
 config_database()
 
+
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(product_router, prefix='/products', tags=['Product'])
 
@@ -27,16 +29,8 @@ app.add_middleware(CORSMiddleware,
 # TODO: TEST AWS BUCKET (BOTO3)
 load_dotenv()
 
+
 aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
 bucket_name = os.getenv('BUCKET_NAME')
 
-s3 = boto3.client('s3',
-                  aws_access_key_id=aws_access_key_id,
-                  aws_secret_access_key=aws_secret_access_key)
-
-
-@app.post('/')
-async def upload_file(file: UploadFile = None):
-    s3.upload_fileobj(file.file, bucket_name, file.filename)
-    return file.filename
