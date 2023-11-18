43dc4b7300ab442dc8e6b31ac66fea1a72a0307b
moliv <molivramento@gmail.com>
2023-11-18 11:51:15 -0300

    adding auth

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
@@ -18,8 +16,8 @@ app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
 app.include_router(auth_router, prefix='/auth', tags=['Auth'])
-app.include_router(product_router, prefix='/products', tags=['Products'])
 app.include_router(user_router, prefix='/users', tags=['Users'])
+app.include_router(product_router, prefix='/products', tags=['Products'])
 app.include_router(transation_router, prefix='/transation', tags=['Transations'])
 
 origins = ['http://localhost:9000',
@@ -30,12 +28,3 @@ app.add_middleware(CORSMiddleware,
                    allow_credentials=True,
                    allow_methods=["*"],
                    allow_headers=["*"])
-
-# TODO: TEST AWS BUCKET (BOTO3)
-load_dotenv()
-
-
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID')
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
-bucket_name = os.getenv('BUCKET_NAME')
-
