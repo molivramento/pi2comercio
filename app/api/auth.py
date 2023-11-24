a1b1b3f364943a23b37f709be45bbac03bb90284
molivramento <molivramento@gmail.com>
2023-11-24 02:14:41 -0300

    fix me

--- app/api/auth.py
+++ app/api/auth.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException, status
 
 from app.services.security import create_token, get_current_user
 from app.schemas.users import UserFilter, UserOut
@@ -11,12 +11,16 @@ router = APIRouter()
 
 
 @router.post("/token")
-async def get_token(data: OAuth2PasswordRequestForm = Depends()):
-    user = await user_service.get(UserFilter(email=data.username))
-    return await create_token(data={'sub': str(user[0].uuid),
-                                    'email': user[0].email,
-                                    'is_staff': user[0].is_staff,
-                                    'is_superuser': user[0].is_superuser})
+async def get_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = await user_service.get(UserFilter(email=form_data.username))
+    try:
+        return await create_token(data={'sub': str(user[0].uuid),
+                                        'email': user[0].email,
+                                        'is_staff': user[0].is_staff,
+                                        'is_superuser': user[0].is_superuser})
+    except IndexError:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with email {form_data.username} not found")
 
 
 @router.get("/me", response_model=UserOut)
