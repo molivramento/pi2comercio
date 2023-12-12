fbe94ee0bf0dd76cbbd81f5559c511db5e196914
david nascimento <viniciosdavid1@gmail.com>
2023-12-12 10:59:22 -0300

    merge places and bills

--- main.py
+++ main.py
@@ -9,11 +9,8 @@ from app.api.users import router as user_router
 from app.api.auth import router as auth_router
 from app.api.transaction import router as transaction_router
 from app.api.itens import router as itens_router
-<<<<<<< HEAD
 from app.api.places import router as places_router
-=======
 from app.api.bills import router as bill_router
->>>>>>> ac63f1e24403bb2411804f8ff8a22883eb9df0b1
 
 config_database()
 
@@ -26,11 +23,8 @@ app.include_router(user_router, prefix='/users', tags=['Users'])
 app.include_router(product_router, prefix='/products', tags=['Products'])
 app.include_router(transaction_router, prefix='/transation', tags=['Transations'])
 app.include_router(itens_router, prefix='/itens', tags=['Itens'])
-<<<<<<< HEAD
 app.include_router(places_router, prefix='/places', tags=['Places'])
-=======
 app.include_router(bill_router, prefix='/bills', tags=['Bills'])
->>>>>>> ac63f1e24403bb2411804f8ff8a22883eb9df0b1
 
 origins = ['http://localhost:9000',
            'http://127.0.0.1:9000'
